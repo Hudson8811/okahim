16972ad710da3319d24e6564b3e14ba54f218d1b
PipZip <37911683+PipZip@users.noreply.github.com>
2019-09-30 19:16:15 +0600

    buttons

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,9 @@
 //= libs/jquery-3.3.1.min.js
 //= libs/jquery.fancybox.min.js
 //= libs/jquery.maskedinput.min.js
+//= libs/TweenMax.min.js
 
 /* my scripts */
 
 //= partials/main.js
+//= partials/btn.js
