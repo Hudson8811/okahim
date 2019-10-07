662d38017a3a297e1c9d8fa0a6db868b96da8324
PipZip <37911683+PipZip@users.noreply.github.com>
2019-10-07 16:29:51 +0600

    product

--- build/js/main.js
+++ build/js/main.js
@@ -352,3 +352,40 @@ Array.prototype.forEach.call( inputs, function( input )
 $(document).ready(function() {
 
 });
+$(document).ready(function() {
+
+    
+    $('.vendor-block .show-more').click(function () {
+        event.preventDefault();
+        var vendor = $(this).parents('.vendor-block');
+        if ($(vendor).hasClass('active')){
+            $(vendor).find('.tech-table, .vendor-gallery').slideUp();
+            $(vendor).removeClass('active');
+            $(this).find('.label').html($(this).find('.label').html() == 'Подробнее' ? 'Свернуть' : 'Подробнее');
+        } else {
+            $(vendor).find('.tech-table, .vendor-gallery').slideDown();
+            $(vendor).addClass('active');
+            $(this).find('.label').html($(this).find('.label').html() == 'Подробнее' ? 'Свернуть' : 'Подробнее');
+            activateVendoeSwiper(vendor);
+        }
+    })
+});
+
+function activateVendoeSwiper(elem) {
+    $(elem).find($(".vendor-gallery:not('.active')")).each(function(index, element){
+        $(this).addClass('active');
+        var $this = $(this);
+        var swiper = new Swiper(this, {
+
+            loop: true,
+            pagination: {
+                el: $this.find(".swiper-pagination")[0],
+                clickable: true,
+            },
+            navigation: {
+                nextEl: $this.find(".swiper-button-next")[0],
+                prevEl: $this.find(".swiper-button-prev")[0],
+            }
+        });
+    });
+}
