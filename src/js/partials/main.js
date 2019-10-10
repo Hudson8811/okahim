97ad424670de192cca5e208569891fc9289cce0e
PipZip <37911683+PipZip@users.noreply.github.com>
2019-10-10 15:56:49 +0600

    check

--- src/js/partials/main.js
+++ src/js/partials/main.js
@@ -2,5 +2,25 @@ $(document).ready(function() {
     $("input[name='phone']").mask(" +7 (999) 999-99-99");
 
 
+    function falidator(item) {
+        check = true;
+        $(item).find('.required').each(function () {
+            if ($(this).hasClass('required') && $(this).val() == '') {
+                check = false;
+                $(this).addClass('error');
+            } else {
+                $(this).removeClass('error');
+            }
+        });
+        if (check) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    $(".modal-form form").submit(function () {
+        if (!falidator(this)) return false;
+    });
 
 });
